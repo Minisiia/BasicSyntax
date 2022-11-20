3230bf6a28987178d41cee60734598854b5fcfe3
Minisiia <ar_tik@yahoo.com>
2022-11-20 18:10:39 +0200

    Java Start. 3. Data Types and Variables. Basic Syntax

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int uberflu?; // у назві недопустимий символ ?
+        int  _Identifier; // допустима назва змінної
+        int \u006fIdentifier; // у назві недопустимий символ \
+        int &myVar; // у назві недопустимий символ &
+        int myVariab1le; // допустима назва змінної
+    }
+}
